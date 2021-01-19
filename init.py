bfb97e9f7f1121b82d88ea78e8f3826440a75c40
hadeer essam <hadeeressamanwe1997@gmail.com>
2021-01-19 20:54:43 +0200

    loss_fn

--- init.py
+++ init.py
@@ -4,3 +4,7 @@ from data_loader import *
 def init():
     download_mnist()
     save_mnist()
+def get_batch(X, Y, batch_size):
+    N = len(X)
+    i = random.randint(1, N-batch_size)
+    return X[i:i+batch_size], Y[i:i+batch_size]
---
+++ visualization.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_losses(losses):
+    t = np.arange(len(losses))
+    plt.plot(t, losses)
+    plt.show()
